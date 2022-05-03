a93b6240c9f8e851edc4c5d024503fd70d444ea2
LaraKra <konlor@rambler.ru>
2022-05-03 22:49:14 +0300

    add test

---
+++ src/test/java/ru/netology/AuthorizationTest.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class AuthorizationTest {
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999/");
+}
+
