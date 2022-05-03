035d2f410d27bd9c4055b072a62ac9b3ab87b2ac
LaraKra <konlor@rambler.ru>
2022-05-03 22:59:27 +0300

    change build

--- src/test/java/ru/netology/AuthorizationTest.java
+++ src/test/java/ru/netology/AuthorizationTest.java
@@ -2,10 +2,13 @@ package ru.netology;
 
 import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class AuthorizationTest {
     @BeforeEach
     public void setUp() {
         open("http://localhost:9999/");
+    }
 }
 
